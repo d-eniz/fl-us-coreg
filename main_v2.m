68e012f4762e58465c2e0922883bc3007f5aae7e
d-eniz <zcemdte@ucl.ac.uk>
2024-01-06 14:26:39 +0000

    added mass file processing capability

--- main_v2.m
+++ main_v2.m
@@ -1,18 +1,27 @@
+%% Initialize
+
 clear
 close all
+addpath("funcs\")
 
 %% Settings
 
-fname = ["water30int20.h5"]; % file name
-
-angle = 0; % add if known, no impact on image processing
 threshold = 100;
 visibility = 50; % new setting, helps a lot with visibility
 
+data_folder = "C:\Users\Deniz\Desktop\data";
+addpath(data_folder);
+fileList = dir(fullfile(data_folder, "*.h5"));
+
+%% Start loop to process all files in folder
+
+for i = 1:length(fileList)
+    currentFile = fullfile(data_folder, fileList(i).name);
+    [~, name, format] = fileparts(char(currentFile));
+    fname = [name, format];
+
 %% Setup
 
-addpath("C:\Users\Deniz\Desktop\data");
-addpath("funcs\")
 US_data = H5toUS(fname);
 FL_data = H5toFL(fname);
 scan_depth = h5readatt(fname, '/', 'depth_mm');
@@ -47,11 +56,17 @@ depth = (depth .* scan_depth) ./ size(US_data, 1); % add dimensions
 
 %% Fluorescence depth correction
 
-FL_processed = richardFL(depth); % richard's method - works very well on 30 degrees, not as well on 45
-%FL_processed = denizFL(depth); % my (old) method, i kept it because its useful to demonstrate a bad example
+FL_processed = richardFL(depth); % richard's method - works generally well
+%FL_processed = denizFL(depth); % my (old) method, doesn't work at all but
+% i kept it because it's useful to demonstrate a bad example
 
+% This part is about displaying how much the experimental data deviates
+% from the model values calculated using depth data. I will experiment on
+% which mode to use to display this deviation.
 depth_corrected = sqrt((FL_data - FL_processed).^2);
-depth_corrected = 1 - max(depth_corrected) + depth_corrected; % shift so that max is at 1
+depth_corrected = 0.5 - mean(depth_corrected) + depth_corrected; % shift median to 0.5
+%depth_corrected = 1 - max(depth_corrected) + depth_corrected; % shift max to 1
+
 FL_image = createFLcmap(depth_corrected, "jet");
 FL_image = imresize(FL_image, [size(US_data, 1), size(US_data, 2)]);
 
@@ -60,14 +75,19 @@ FL_image = imresize(FL_image, [size(US_data, 1), size(US_data, 2)]);
 x_axis = dx:dx:scan_length;
 y_axis = dy:dy:scan_depth;
 
-figure("WindowState","maximized");
+data_plots = figure;
 subplot(2,2,1);
+sgtitle(fname)
     plot(x_axis, depth_corrected)
+    xlim("tight")
+    if max(depth_corrected) > 1
+        ylim([0 max(depth_corrected)])
+    else 
+        ylim([0 1])
+    end
     title("Fluorescence, depth corrected")
-    ylim([0 1])
     xlabel("Distance (cm)"),
     ylabel("Fluorescence (AU)")
-    axis tight
 subplot(2,2,3);
     scatter(depth, FL_data,".")
     title("Fluorescence, experimental vs model")
@@ -80,7 +100,7 @@ subplot(2,2,3);
     axis tight
 subplot(2,2,2);
     imagesc(FL_image)
-    title("Fluorescence, depth corrected (colormap)")
+    title("Colormap")
     colorbar
     axis tight
 subplot(2,2,4)
@@ -89,11 +109,12 @@ subplot(2,2,4)
     title("Detected surface")
     xlabel("Distance (cm)"),
     ylabel("Depth (cm)")
+    axis equal
     axis tight
 
 %% Coregistration
 
-figure("WindowState","maximized");
+coregistered_img = figure;
     imagesc(x_axis, y_axis, US_data)
     hold on
     colormap gray
@@ -107,4 +128,23 @@ figure("WindowState","maximized");
     ylabel("Depth (cm)")
     axis equal
     axis tight
+
+%% Save images
+
+if ~exist("processed_images", "dir")
+    mkdir("processed_images");
+end
+filename = fullfile("processed_images", name + "_data_plots_t" + threshold + "v" + visibility + ".png");
+saveas(data_plots, filename, "png");
+disp("Image saved at: " + filename);
+filename = fullfile("processed_images", name + "_coregistered_img_t" + threshold + "v" + visibility + ".png");
+saveas(coregistered_img, filename, "png");
+disp("Image saved at: " + filename);
+
+clf
+close all
+
+end % Image processing loop end
+disp("All images saved")
+
 %}
